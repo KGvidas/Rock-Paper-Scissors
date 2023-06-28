07532676625a8b7b5c62c2f501fb78044e09657c
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-06-28 23:36:45 +0300

    Add text to console, to show computer choice

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
     let computerChoise = Math.floor(Math.random() * choices.length);
-    console.log(choices[computerChoise]);
+    return choices[computerChoise];
 };
-getComputerChoice();
+console.log("Computer choice is: " + getComputerChoice());
