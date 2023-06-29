f73f1d5d193725a8612aae4a00c04ff1c76157dd
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-06-29 14:32:26 +0300

    Add playRound function

--- javascript.js
+++ javascript.js
@@ -1,7 +1,33 @@
 function getComputerChoice() {
-    const choices = ["Rock", "Paper", "Scissors"];
+    const choices = ["rock", "paper", "scissors"];
     let randomNumber = Math.floor(Math.random() * choices.length);
     let computerChoice = choices[randomNumber];
     return computerChoice;
 };
-console.log("Computer choice is: " + getComputerChoice());
+
+
+function playRound(playerSelection, computerSelection) {
+  if (
+  (playerSelection === "rock" && computerSelection === "scissors") ||
+  (playerSelection === "paper" && computerSelection === "rock") ||
+  (playerSelection === "scissors" && computerSelection === "paper")
+  ){
+    return "You win! " + playerSelection + " beats " + computerSelection;
+  } else if (
+  (playerSelection === "rock" && computerSelection === "paper") ||
+  (playerSelection === "paper" && computerSelection === "scissors") ||
+  (playerSelection === "scissors" && computerSelection === "rock")
+  ) { 
+    return "You lose! " + computerSelection + " beats " + playerSelection;
+  } else {
+    return "It's a tie!";
+  }
+}
+
+ 
+const playerSelection = "rock";
+console.log("Player choice is: " + playerSelection);
+
+const computerSelection = getComputerChoice();
+console.log("Computer choise is: " + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
