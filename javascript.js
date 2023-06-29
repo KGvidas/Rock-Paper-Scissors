0c8ac7cef3379a804233ef82efe9aa29598f2da8
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-06-29 14:12:21 +0300

    Fix getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 function getComputerChoice() {
     const choices = ["Rock", "Paper", "Scissors"];
-    let computerChoice = Math.floor(Math.random() * choices.length);
-    return choices[computerChoice];
+    let randomNumber = Math.floor(Math.random() * choices.length);
+    let computerChoice = choices[randomNumber];
+    return computerChoice;
 };
 console.log("Computer choice is: " + getComputerChoice());
