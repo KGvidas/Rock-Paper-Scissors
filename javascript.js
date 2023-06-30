2c38d02dca8a2623efa4ece2891b8ea3f0b3797f
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-06-30 07:52:42 +0300

    Add incrementation to wins count, after each round

--- javascript.js
+++ javascript.js
@@ -12,26 +12,21 @@ function playRound(playerSelection, computerSelection) {
   (playerSelection === "paper" && computerSelection === "rock") ||
   (playerSelection === "scissors" && computerSelection === "paper")
   ){
-    return "You win! " + playerSelection + " beats " + computerSelection;
+    console.log("You win! " + playerSelection + " beats " + computerSelection);
+    playerWins++;
   } else if (
   (playerSelection === "rock" && computerSelection === "paper") ||
   (playerSelection === "paper" && computerSelection === "scissors") ||
   (playerSelection === "scissors" && computerSelection === "rock")
   ) { 
-    return "You lose! " + computerSelection + " beats " + playerSelection;
+    console.log("You lose! " + computerSelection + " beats " + playerSelection);
+    computerWins++;
   } else {
-    return "It's a tie!";
+    return console.log("It's a tie!");
   }
 }
 
- 
-let playerSelection = prompt("Choose between Rock, Paper and Scissors").toLowerCase();
-console.log("Player choice is: " + playerSelection);
 
-const computerSelection = getComputerChoice();
-console.log("Computer choice is: " + computerSelection);
-
-console.log(playRound(playerSelection, computerSelection));
 
 let playerWins = 0;
 let computerWins = 0;
