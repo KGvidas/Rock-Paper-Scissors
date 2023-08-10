8b43194c37ea561f96082d8dc5b3dcebe829406e
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-08-10 08:40:30 +0300

    Add blank css file

--- javascript.js
+++ javascript.js
@@ -33,19 +33,20 @@ let computerWins = 0;
 
 function game() {
   while (playerWins < 5 && computerWins < 5) {
-  let playerSelection = prompt("Choose between Rock, Paper and Scissors").toLowerCase();
-  console.log("Player choice is: " + playerSelection);
-  let computerSelection = getComputerChoice();
-  console.log("Computer choice is: " + computerSelection);
+    let playerSelection = prompt("Choose between Rock, Paper and Scissors").toLowerCase();
+    console.log("Player choice is: " + playerSelection);
+    let computerSelection = getComputerChoice();
+    console.log("Computer choice is: " + computerSelection);
 
-  playRound(playerSelection, computerSelection);
-  console.log("Player has: " + playerWins + " wins");
-  console.log("Computer has " + computerWins + " wins");
-} 
+    playRound(playerSelection, computerSelection);
+    console.log("Player has: " + playerWins + " wins");
+    console.log("Computer has " + computerWins + " wins");
+  }  
+  
   if (playerWins === 5) {
   alert("Congratz you won!");
-} else if (computerWins === 5) {
+  } else if (computerWins === 5) {
   alert("Wow you lost!");
-}
+  }
 }
 game();
