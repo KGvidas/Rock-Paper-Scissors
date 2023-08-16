6841142335c942ded0774d772544859d146066fa
KGvidas <122453305+KGvidas@users.noreply.github.com>
2023-08-16 16:22:03 +0300

    Add gameOver and playAgain functions after 5rounds

--- javascript.js
+++ javascript.js
@@ -1,75 +1,7 @@
-// function getComputerChoice() {
-//     const choices = ["rock", "paper", "scissors"];
-//     let randomNumber = Math.floor(Math.random() * choices.length);
-//     let computerChoice = choices[randomNumber];
-//     return computerChoice;
-// };
-
-// const buttons = document.querySelectorAll('button');
-
-// buttons.forEach((button) => {
-//   button.addEventListener('click', playerSelection);
-// });
-
-// // const btnRock = document.getElementById('rock');
-// // btnRock.addEventListener('click', () => {
-// //     console.log(btnRock.id);
-// // });
-
-// function playerSelection () {
-// let playerChoice = buttons.id;
-// return console.log(playerChoice);
-// }
-
-
-
-
-// function playRound(playerSelection, getComputerChoice) {
-//   if (
-//   (playerSelection === "rock" && computerSelection === "scissors") ||
-//   (playerSelection === "paper" && computerSelection === "rock") ||
-//   (playerSelection === "scissors" && computerSelection === "paper")
-//   ){
-//     console.log("You win! " + playerSelection + " beats " + computerSelection);
-//     playerWins++;
-//   } else if (
-//   (playerSelection === "rock" && computerSelection === "paper") ||
-//   (playerSelection === "paper" && computerSelection === "scissors") ||
-//   (playerSelection === "scissors" && computerSelection === "rock")
-//   ) { 
-//     console.log("You lose! " + computerSelection + " beats " + playerSelection);
-//     computerWins++;
-//   } else {
-//     return console.log("It's a tie!" + "" + playerSelection);
-//   }
-// }
-
-
-// // function game() {
-// //   while (playerWins < 5 && computerWins < 5) {
-// //     let playerSelection = prompt("Choose between Rock, Paper and Scissors").toLowerCase();
-// //     console.log("Player choice is: " + playerSelection);
-// //     let computerSelection = getComputerChoice();
-// //     console.log("Computer choice is: " + computerSelection);
-
-// //     playRound(playerSelection, computerSelection);
-// //     console.log("Player has: " + playerWins + " wins");
-// //     console.log("Computer has " + computerWins + " wins");
-// //   }  
-  
-// //   if (playerWins === 5) {
-// //   alert("Congratz you won!");
-// //   } else if (computerWins === 5) {
-// //   alert("Wow you lost!");
-// //   }
-// // }
-// // game();
-
-
 let playerWins = 0;
 let computerWins = 0;
 
-const buttons = document.querySelectorAll('button');
+const buttons = document.querySelectorAll('.play');
 buttons.forEach((button) => {
   button.addEventListener('click', () => {
     const playerSelection = button.id; // Get the id of the clicked button
@@ -79,8 +11,10 @@ buttons.forEach((button) => {
     // Check for game end condition
     if (playerWins === 5) {
       alert("Congrats, you won!");
+      gameIsOver();
     } else if (computerWins === 5) {
       alert("Wow, you lost!");
+      gameIsOver();
     }
   });
 });
@@ -133,15 +67,16 @@ function playRound(playerSelection, computerSelection) {
   } 
 }
 
-function game() {
-  while (playerWins < 5 && computerWins < 5) {
-    playRound(playerSelection, computerSelection);
-  }  
-  if (playerWins === 5) {
-  alert("Congratz you won!");
-  } else if (computerWins === 5) {
-  alert("Wow you lost!");
-  }
+gameOverbtn = document.getElementById('text')
+gameOverbtn.addEventListener('click', playAgain);
+
+
+function gameIsOver() {
+  document.getElementById("overlay").style.display = "block";
+}
+
+function playAgain() {
+  document.getElementById("overlay").style.display = "none";
 }
 
 // Improving Code Organization:
